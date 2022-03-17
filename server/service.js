73d3dd2461356a481e03ab72953a0a6f7c1e6a5c
Vitor Escalfoni <vitor.hme@gmail.com>
2022-03-16 23:53:58 -0300

    created server and configured routes

---
+++ server/service.js
@@ -0,0 +1,34 @@
+import fs from "fs";
+import fsPromises from "fs/promises";
+import config from "./config.js";
+import { join, extname } from "path";
+
+const {
+  dir: { publicDirectory },
+} = config;
+
+export class Service {
+  createFileStream(filename) {
+    return fs.createReadStream(filename);
+  }
+
+  async getFileInfo(file) {
+    // file = home/index.html
+    const fullFilePath = join(publicDirectory, file);
+    // check if exists, otherwise will throw error!!
+    await fsPromises.access(fullFilePath);
+    const fileType = extname(fullFilePath);
+    return {
+      type: fileType,
+      name: fullFilePath,
+    };
+  }
+
+  async getFileStream(file) {
+    const { name, type } = await this.getFileInfo(file);
+    return {
+      stream: this.createFileStream(name),
+      type,
+    };
+  }
+}
