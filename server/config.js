2bb763059a392ef1e86e8b6c16c4e0dda18cdeda
Vitor Escalfoni <vitor.hme@gmail.com>
2022-03-20 11:58:17 -0300

    added configs and improved error handling

--- server/config.js
+++ server/config.js
@@ -21,4 +21,11 @@ export default {
   location: {
     home: "/home",
   },
+  constants: {
+    CONTENT_TYPE: {
+      ".html": "text/html",
+      ".css": "text/css",
+      ".js": "text/javascript",
+    },
+  },
 };
